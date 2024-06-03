97a6dea8d66abe0bc76aad66abda3d0d0b4760e3
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2024-06-03 23:26:51 +0900

    404 오류 등록

--- masterpiece/__init__.py
+++ masterpiece/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -12,6 +12,9 @@ naming_convention = {
 }
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 def create_app():
     app = Flask(__name__)
@@ -36,5 +39,8 @@ def create_app():
     from .filters import format_datetime, round2
     app.jinja_env.filters['datetime'] = format_datetime
     app.jinja_env.filters['round2'] = round2
+
+    # 오류페이지
+    app.register_error_handler(404, page_not_found)
 
     return app
