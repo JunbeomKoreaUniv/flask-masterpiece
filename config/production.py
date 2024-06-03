63bc5635555bf796639cd5487ee760f1f3ce0f60
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2024-06-03 22:28:30 +0900

    config 변경

--- config/production.py
+++ config/production.py
@@ -1,7 +1,5 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'masterpiece.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
+SECRET_KEY = b'\xec\x14\x00\xe9\xc6FH\xf7\xf6\xe3\x06\xb0l.\xb9z'
